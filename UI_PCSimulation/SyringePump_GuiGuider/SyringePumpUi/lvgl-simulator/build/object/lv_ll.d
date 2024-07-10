9dc00009c785a64ecec287a707ec6355086ed700
Mehdi Bagherimofidi <mehdi.bagherimofidi@yahoo.com>
2024-07-10 10:41:16 +0330

    compile Guider simulator after Bolus

--- UI_PCSimulation/SyringePump_GuiGuider/SyringePumpUi/lvgl-simulator/build/object/lv_ll.d
+++ UI_PCSimulation/SyringePump_GuiGuider/SyringePumpUi/lvgl-simulator/build/object/lv_ll.d
@@ -1,23 +1,23 @@
 build/object/lv_ll.o: \
- e:\designs\javad_works\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\misc\lv_ll.c \
- e:\designs\javad_works\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\misc\lv_ll.h \
- e:\designs\javad_works\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\misc\lv_mem.h \
- e:\designs\javad_works\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\lv_conf_internal.h \
- e:\designs\javad_works\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\lv_conf_kconfig.h \
- E:/Designs/Javad_works/SyringePump/UI_PCSimulation/SyringePump_GuiGuider/SyringePumpUi/lvgl-simulator/lv_conf.h \
- e:\designs\javad_works\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\custom\lv_conf_ext.h \
- e:\designs\javad_works\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\misc\lv_types.h
+ e:\workspace\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\misc\lv_ll.c \
+ e:\workspace\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\misc\lv_ll.h \
+ e:\workspace\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\misc\lv_mem.h \
+ e:\workspace\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\lv_conf_internal.h \
+ e:\workspace\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\lv_conf_kconfig.h \
+ E:/Workspace/SyringePump/UI_PCSimulation/SyringePump_GuiGuider/SyringePumpUi/lvgl-simulator/lv_conf.h \
+ e:\workspace\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\custom\lv_conf_ext.h \
+ e:\workspace\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\misc\lv_types.h
 
-e:\designs\javad_works\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\misc\lv_ll.h:
+e:\workspace\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\misc\lv_ll.h:
 
-e:\designs\javad_works\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\misc\lv_mem.h:
+e:\workspace\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\misc\lv_mem.h:
 
-e:\designs\javad_works\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\lv_conf_internal.h:
+e:\workspace\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\lv_conf_internal.h:
 
-e:\designs\javad_works\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\lv_conf_kconfig.h:
+e:\workspace\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\lv_conf_kconfig.h:
 
-E:/Designs/Javad_works/SyringePump/UI_PCSimulation/SyringePump_GuiGuider/SyringePumpUi/lvgl-simulator/lv_conf.h:
+E:/Workspace/SyringePump/UI_PCSimulation/SyringePump_GuiGuider/SyringePumpUi/lvgl-simulator/lv_conf.h:
 
-e:\designs\javad_works\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\custom\lv_conf_ext.h:
+e:\workspace\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\custom\lv_conf_ext.h:
 
-e:\designs\javad_works\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\misc\lv_types.h:
+e:\workspace\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\misc\lv_types.h:
--- UI_PCSimulation/SyringePump_GuiGuider/SyringePumpUi/lvgl-simulator/build/object/lv_style_gen.d
+++ UI_PCSimulation/SyringePump_GuiGuider/SyringePumpUi/lvgl-simulator/build/object/lv_style_gen.d
@@ -1,65 +1,65 @@
 build/object/lv_style_gen.o: \
- e:\designs\javad_works\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\misc\lv_style_gen.c \
- e:\designs\javad_works\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\misc\lv_style.h \
- e:\designs\javad_works\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\font\lv_font.h \
- e:\designs\javad_works\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\lv_conf_internal.h \
- e:\designs\javad_works\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\lv_conf_kconfig.h \
- E:/Designs/Javad_works/SyringePump/UI_PCSimulation/SyringePump_GuiGuider/SyringePumpUi/lvgl-simulator/lv_conf.h \
- e:\designs\javad_works\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\custom\lv_conf_ext.h \
- e:\designs\javad_works\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\font\lv_symbol_def.h \
- e:\designs\javad_works\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\misc\lv_area.h \
- e:\designs\javad_works\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\lv_conf_internal.h \
- e:\designs\javad_works\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\misc\lv_color.h \
- e:\designs\javad_works\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\misc\lv_assert.h \
- e:\designs\javad_works\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\misc\lv_log.h \
- e:\designs\javad_works\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\misc\lv_types.h \
- e:\designs\javad_works\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\misc\lv_mem.h \
- e:\designs\javad_works\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\misc\lv_math.h \
- e:\designs\javad_works\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\misc\lv_area.h \
- e:\designs\javad_works\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\misc\lv_anim.h \
- e:\designs\javad_works\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\misc\lv_txt.h \
- e:\designs\javad_works\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\misc\lv_printf.h \
- e:\designs\javad_works\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\misc\lv_bidi.h \
- e:\designs\javad_works\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\misc\lv_style_gen.h
+ e:\workspace\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\misc\lv_style_gen.c \
+ e:\workspace\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\misc\lv_style.h \
+ e:\workspace\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\font\lv_font.h \
+ e:\workspace\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\lv_conf_internal.h \
+ e:\workspace\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\lv_conf_kconfig.h \
+ E:/Workspace/SyringePump/UI_PCSimulation/SyringePump_GuiGuider/SyringePumpUi/lvgl-simulator/lv_conf.h \
+ e:\workspace\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\custom\lv_conf_ext.h \
+ e:\workspace\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\font\lv_symbol_def.h \
+ e:\workspace\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\misc\lv_area.h \
+ e:\workspace\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\lv_conf_internal.h \
+ e:\workspace\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\misc\lv_color.h \
+ e:\workspace\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\misc\lv_assert.h \
+ e:\workspace\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\misc\lv_log.h \
+ e:\workspace\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\misc\lv_types.h \
+ e:\workspace\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\misc\lv_mem.h \
+ e:\workspace\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\misc\lv_math.h \
+ e:\workspace\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\misc\lv_area.h \
+ e:\workspace\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\misc\lv_anim.h \
+ e:\workspace\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\misc\lv_txt.h \
+ e:\workspace\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\misc\lv_printf.h \
+ e:\workspace\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\misc\lv_bidi.h \
+ e:\workspace\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\misc\lv_style_gen.h
 
-e:\designs\javad_works\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\misc\lv_style.h:
+e:\workspace\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\misc\lv_style.h:
 
-e:\designs\javad_works\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\font\lv_font.h:
+e:\workspace\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\font\lv_font.h:
 
-e:\designs\javad_works\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\lv_conf_internal.h:
+e:\workspace\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\lv_conf_internal.h:
 
-e:\designs\javad_works\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\lv_conf_kconfig.h:
+e:\workspace\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\lv_conf_kconfig.h:
 
-E:/Designs/Javad_works/SyringePump/UI_PCSimulation/SyringePump_GuiGuider/SyringePumpUi/lvgl-simulator/lv_conf.h:
+E:/Workspace/SyringePump/UI_PCSimulation/SyringePump_GuiGuider/SyringePumpUi/lvgl-simulator/lv_conf.h:
 
-e:\designs\javad_works\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\custom\lv_conf_ext.h:
+e:\workspace\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\custom\lv_conf_ext.h:
 
-e:\designs\javad_works\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\font\lv_symbol_def.h:
+e:\workspace\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\font\lv_symbol_def.h:
 
-e:\designs\javad_works\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\misc\lv_area.h:
+e:\workspace\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\misc\lv_area.h:
 
-e:\designs\javad_works\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\lv_conf_internal.h:
+e:\workspace\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\lv_conf_internal.h:
 
-e:\designs\javad_works\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\misc\lv_color.h:
+e:\workspace\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\misc\lv_color.h:
 
-e:\designs\javad_works\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\misc\lv_assert.h:
+e:\workspace\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\misc\lv_assert.h:
 
-e:\designs\javad_works\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\misc\lv_log.h:
+e:\workspace\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\misc\lv_log.h:
 
-e:\designs\javad_works\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\misc\lv_types.h:
+e:\workspace\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\misc\lv_types.h:
 
-e:\designs\javad_works\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\misc\lv_mem.h:
+e:\workspace\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\misc\lv_mem.h:
 
-e:\designs\javad_works\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\misc\lv_math.h:
+e:\workspace\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\misc\lv_math.h:
 
-e:\designs\javad_works\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\misc\lv_area.h:
+e:\workspace\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\misc\lv_area.h:
 
-e:\designs\javad_works\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\misc\lv_anim.h:
+e:\workspace\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\misc\lv_anim.h:
 
-e:\designs\javad_works\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\misc\lv_txt.h:
+e:\workspace\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\misc\lv_txt.h:
 
-e:\designs\javad_works\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\misc\lv_printf.h:
+e:\workspace\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\misc\lv_printf.h:
 
-e:\designs\javad_works\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\misc\lv_bidi.h:
+e:\workspace\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\misc\lv_bidi.h:
 
-e:\designs\javad_works\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\misc\lv_style_gen.h:
+e:\workspace\syringepump\ui_pcsimulation\syringepump_guiguider\syringepumpui\lvgl\src\misc\lv_style_gen.h:
